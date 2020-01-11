3bc6a2db504d1d3c81985e35d5f27875a84fe312
gorinifran <francesco.gorini2000@yahoo.com>
2020-01-12 02:20:07 +1300

    Adding android folder, signup page talks to server

--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 
+import axios from 'axios';
+import qs from 'qs';
 import styles from './style'
 
 export default class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -16,6 +18,44 @@ export default class Login extends Component {
         }
     }
 
+    showError = (msg) => {
+        this.setState({
+            message: msg, loading: false
+        });
+    }
+
+    signup = async () => {
+        if (this.state.loading) return;
+
+        const { username, password, re_password } = this.state;
+        this.setState({ loading: true });
+
+        if (username === '' || password === '' || re_password === '') {
+            this.showError('Textfields cannot be blank!');
+        } else if (password !== re_password) {
+            this.showError('Passwords do not match!');
+        } else {
+            try {
+                // Send data to server
+                const response = await axios.post('http://10.0.2.2:1234/signup', qs.stringify({
+                    username: username,
+                    password: password
+                }), {
+                    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+                }
+                );
+
+                // No error code thrown. Signup successful
+
+                this.setState({ loading: false });
+                return this.props.navigation.navigate('Login');
+
+            } catch (error) {
+                this.showError(error.response.data);
+            }
+        }
+    }
+
     render() {
         return (
             <View style={styles.wrapper}>
@@ -24,33 +64,36 @@ export default class Login extends Component {
                     <Text style={styles.subTitle}>secure communications</Text>
                 </View>
                 <View style={styles.container}>
-                    { this.state.message ? <Text style={styles.errorMsg}>{this.state.message}</Text> : null}
+                    {this.state.message ? <Text style={styles.errorMsg}>{this.state.message}</Text> : null}
                     <TextInput placeholder="Enter User name"
-                               onChangeText={ TextInputValue =>
-                                   this.setState({username : TextInputValue }) }
-                               underlineColorAndroid='transparent'
-                               style={styles.input}
+                        onChangeText={TextInputValue =>
+                            this.setState({ username: TextInputValue })}
+                        underlineColorAndroid='transparent'
+                        style={styles.input}
                     />
                     <TextInput placeholder="Enter password"
-                               onChangeText={ TextInputValue =>
-                                   this.setState({password: TextInputValue }) }
-                               underlineColorAndroid='transparent'
-                               secureTextEntry={true}
-                               style={styles.input}
+                        onChangeText={TextInputValue =>
+                            this.setState({ password: TextInputValue })}
+                        underlineColorAndroid='transparent'
+                        secureTextEntry={true}
+                        style={styles.input}
                     />
                     <TextInput placeholder="Repeat password"
-                               onChangeText={ TextInputValue =>
-                                   this.setState({re_password: TextInputValue }) }
-                               underlineColorAndroid='transparent'
-                               secureTextEntry={true}
-                               style={styles.input}
+                        onChangeText={TextInputValue =>
+                            this.setState({ re_password: TextInputValue })}
+                        underlineColorAndroid='transparent'
+                        secureTextEntry={true}
+                        style={styles.input}
                     />
-                    <TouchableOpacity style={[styles.button, styles.buttonCyan]}>
-                        <Text style={styles.buttonText}>Signup</Text>
+                    <TouchableOpacity style={[styles.button, styles.buttonCyan]} onPress={() => this.signup()}>
+                        {this.state.loading
+                            ? <ActivityIndicator />
+                            : <Text style={styles.buttonText}>Signup</Text>
+                        }
                     </TouchableOpacity>
                     <Text style={styles.subTitle}>Or</Text>
                     <TouchableOpacity style={styles.button}
-                                      onPress={() => this.props.navigation.navigate('Login')}>
+                        onPress={() => this.props.navigation.navigate('Login')}>
                         <Text style={styles.buttonText}>Login</Text>
                     </TouchableOpacity>
                 </View>
