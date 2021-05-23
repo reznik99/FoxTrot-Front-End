acd72ed07a4a1770781b3a73bc4d333d3acdd818
Francesco <goras.francesco@gmail.com>
2021-05-24 00:02:01 +1200

    Formatting + adding new page to router settings

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-import {Login, Signup, Home, Conversation, NewConversation} from './src'
+import { Login, Signup, Home, Conversation, NewConversation, AddContact } from './src'
 import Header from './src/components/Header';
 import HeaderConversation from "./src/components/HeaderConversation";
 
@@ -14,41 +14,47 @@ export default class App extends React.Component {
     }
 }
 const AppNavigator = createStackNavigator({
-        Home: {
-            screen: Home,
-            navigationOptions: ({ navigation }) => ({
-                title: 'Conversations', //`${navigation.state.params.name}'s Profile'`,
-                header: <Header pageTitle="Conversations" navigation={navigation}/>
-            })
-        }, Conversation: {
-            screen: Conversation,
-            navigationOptions: ({ navigation }) => ({
-                title: 'Someones number/name',
-                header: <HeaderConversation navigation={navigation} data={navigation.state.params.data} allowBack={true}/>
-            })
-        }, NewConversation: {
-            screen: NewConversation,
-            navigationOptions: ({ navigation }) => ({
-                title: 'New Conversation',
-                header: <Header pageTitle="New Conversation" navigation={navigation} allowBack={true}/>
-            })
-        }
-    },
+    Home: {
+        screen: Home,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Conversations', //`${navigation.state.params.name}'s Profile'`,
+            header: <Header pageTitle="Conversations" navigation={navigation} />
+        })
+    }, Conversation: {
+        screen: Conversation,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Someones number/name',
+            header: <HeaderConversation navigation={navigation} data={navigation.state.params.data} allowBack={true} />
+        })
+    }, NewConversation: {
+        screen: NewConversation,
+        navigationOptions: ({ navigation }) => ({
+            title: 'New Conversation',
+            header: <Header pageTitle="New Conversation" navigation={navigation} allowBack={true} />
+        })
+    }, AddContact: {
+        screen: AddContact,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Add Contact',
+            header: <Header pageTitle="Add Contact" navigation={navigation} allowBack={true} />
+        })
+    }
+},
     {
         initialRouteName: "Home"
     });
 
 const AuthNavigator = createStackNavigator({
-        Login: {screen: Login},
-        Signup: {screen: Signup},
-        AppNavigator: {
-            screen: AppNavigator,
-            navigationOptions:  {
-                header: null,
-            }
+    Login: { screen: Login },
+    Signup: { screen: Signup },
+    AppNavigator: {
+        screen: AppNavigator,
+        navigationOptions: {
+            header: null,
         }
-    }, {
-        initialRouteName: "Login"
-    });
+    }
+}, {
+    initialRouteName: "Login"
+});
 
 const AppContainer = createAppContainer(AuthNavigator);
