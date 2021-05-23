ec80c008f727f2abfd53bf5a068e32476821184e
Francesco <goras.francesco@gmail.com>
2021-05-24 00:01:41 +1200

    Add contact page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
-export {default as Login} from "./pages/auth/Login";
-export {default as Signup} from "./pages/auth/Signup";
-export {default as Home} from "./pages/home/Home";
-export {default as Conversation} from "./pages/conversation/Conversation";
-export {default as NewConversation} from "./pages/conversation/NewConversation";
+export { default as Login } from "./pages/auth/Login";
+export { default as Signup } from "./pages/auth/Signup";
+export { default as Home } from "./pages/home/Home";
+export { default as Conversation } from "./pages/conversation/Conversation";
+export { default as NewConversation } from "./pages/conversation/NewConversation";
+export { default as AddContact } from "./pages/conversation/AddContact";
