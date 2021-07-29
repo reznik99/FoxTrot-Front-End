f7041e65a47af1853203d5515feaa7e61453b117
Francesco <goras.francesco@gmail.com>
2021-07-30 01:09:08 +1200

    ValidateToken action

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,6 +1,28 @@
 
 import '../../global/variables';
 
+
+export function validateToken() {
+    try {
+        return async (dispatch) => {
+            const res = await axios.get(`${API_URL}/validateToken`);
+            if (res.data?.valid) {
+                dispatch({
+                    type: "TOKEN_VALID",
+                    payload: true,
+                });
+            } else {
+                dispatch({
+                    type: "TOKEN_INVALID",
+                    payload: false,
+                });
+            }
+        };
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 export function getSomeData() {
     try {
         return async (dispatch) => {
