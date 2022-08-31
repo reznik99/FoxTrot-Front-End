c93bccf7a5bbafb0277b04e49548bdb97b6c460a
Francesco <goras.francesco@gmail.com>
2022-09-01 03:59:52 +1200

    I hate react native

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
 import React from 'react'
 import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
