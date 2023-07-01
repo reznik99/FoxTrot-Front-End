fd13d36eab7a4def85dcdcfa35fe6a5880e338c2
Francesco <goras.francesco@gmail.com>
2023-07-02 03:57:13 +1200

    Added dialog for viewing Security Code for a conversation.

--- src/components/HeaderConversation.js
+++ src/components/HeaderConversation.js
@@ -1,47 +1,81 @@
-import React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import React, { useState, useCallback } from 'react'
+import { View, TouchableOpacity } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faVideo, faPhone, faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faVideo, faPhone, faBars, faArrowLeft, faLock } from '@fortawesome/free-solid-svg-icons'
+import { ActivityIndicator, Text, Button, Chip, Dialog, Paragraph, Portal } from 'react-native-paper'
 import { Image } from "react-native-elements"
-import { ActivityIndicator } from 'react-native-paper'
 
 import styles from "./HeaderStyles"
+import { useSelector } from 'react-redux'
+import { publicKeyFingerprint } from '~/global/crypto'
 
 export default function HeaderConversation(props) {
 
-    const { navigation } = props;
+    const { navigation, allowBack, data } = props;
+    const [visibleDialog, setVisibleDialog] = useState('')
+    const [securityCode, setSecurityCode] = useState('')
+    const contacts = useSelector((store) => store.userReducer.contacts)
+
+    const showSecurityCode = useCallback(async () => {
+        const contact = contacts.find(contact => contact.phone_no === data.peer_user.phone_no)
+        if (!contact) return
+        setVisibleDialog('SecurityCode')
+        const digest = await publicKeyFingerprint(contact.public_key)
+        setSecurityCode(digest)
+    })
 
     return (
         <View style={styles.topBar}>
             <View style={styles.backAndTitle}>
                 {
-                    props.allowBack ?
+                    allowBack ?
                         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
                             <FontAwesomeIcon icon={faArrowLeft} size={20} style={styles.topBarText} />
                         </TouchableOpacity>
                         :
                         null
                 }
-                <TouchableOpacity style={styles.profileBtn}>
+                <TouchableOpacity style={styles.profileBtn} onPress={showSecurityCode}>
                     <View style={styles.profilePicContainer}>
-                        <Image source={{ uri: props.data?.peer_user?.pic }}
+                        <Image source={{ uri: `${data?.peer_user?.pic}?size=50x50` }}
                             style={styles.profilePic}
                             PlaceholderContent={<ActivityIndicator />} />
                     </View>
-                    <Text style={styles.topBarText}>{props.data?.peer_user?.phone_no}</Text>
+                    <Text style={styles.topBarText}>{data?.peer_user?.phone_no}</Text>
                 </TouchableOpacity>
             </View>
             <View style={[styles.buttonContainer]}>
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Call', { data: {peer_user: props.data?.peer_user} })}>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Call', { data: { peer_user: data?.peer_user } })}>
                     <FontAwesomeIcon icon={faVideo} size={20} style={styles.topBarText} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Call', { data: {peer_user: props.data?.peer_user} })}>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Call', { data: { peer_user: data?.peer_user } })}>
                     <FontAwesomeIcon icon={faPhone} size={20} style={styles.topBarText} />
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button}>
                     <FontAwesomeIcon icon={faBars} size={20} style={styles.topBarText} />
                 </TouchableOpacity>
             </View>
+
+            <Portal>
+                <Dialog visible={visibleDialog === 'SecurityCode'} onDismiss={() => setVisibleDialog('')}>
+                    <Dialog.Title>
+                        <Text> <FontAwesomeIcon icon={faLock} color="#00ff00" /> Security Code    </Text>
+                        <Paragraph>
+                            <Image source={{ uri: `${data?.peer_user?.pic}?size=40x40` }} style={styles.profilePic} PlaceholderContent={<ActivityIndicator />} />
+                            {data?.peer_user?.phone_no}
+                        </Paragraph>
+                    </Dialog.Title>
+                    <Dialog.Content>
+                        <Paragraph>Verify with your contact that this code matches their profile code:</Paragraph>
+                        {securityCode.match(/.{1,24}/g)?.map(val => (
+                            <Paragraph style={{ fontFamily: 'Roboto', textAlign: 'center' }}>{val}</Paragraph>
+                        ))}
+                    </Dialog.Content>
+                    <Dialog.Actions style={{ justifyContent: 'space-evenly' }}>
+                        <Button onPress={() => setVisibleDialog('')} mode='contained' style={{ paddingHorizontal: 15 }}>OK</Button>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
         </View>
     )
 }
